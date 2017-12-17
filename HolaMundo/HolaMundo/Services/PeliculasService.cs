2e9f00f8ffa4c2c1adc66e0934c70d4036335e25
ReinaldoChavez <32046034+ReinaldoChavez@users.noreply.github.com>
2017-12-16 22:29:08 -0300

    ss

---
+++ HolaMundo/HolaMundo/Services/PeliculasService.cs
@@ -0,0 +1,43 @@
+﻿using HolaMundo.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace HolaMundo.Services
+{
+    public class PeliculasService
+    {
+        public Pelicula ObtenerPelicula()
+        {
+            return new Pelicula()
+            {
+                Titulo = "Jack el destripador",
+                Duracion = 123,
+                Pais = "USA",
+                Publicacion = new DateTime(2013, 12, 3)
+            };
+        }
+
+        public List<Pelicula> ObtenerPeliculas()
+        {
+            var pelicula1 = new Pelicula()
+            {
+                Titulo = "Jack el destripador",
+                Duracion = 123,
+                Pais = "USA",
+                Publicacion = new DateTime(2013, 12, 3)
+            };
+
+            var pelicula2 = new Pelicula()
+            {
+                Titulo = "El Trauco",
+                Duracion = 163,
+                Pais = "Chile",
+                Publicacion = new DateTime(2016, 04, 5)
+            };
+
+            return new List<Pelicula> { pelicula1, pelicula2 };
+        }
+    }
+}
